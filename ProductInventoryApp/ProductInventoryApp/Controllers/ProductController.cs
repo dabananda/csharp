9f7c1dd03d7810446a3b3a664f757c444692b801
dabananda <imdmitra@gmail.com>
2024-12-18 01:09:13 +0600

    Implemented GET products method and views

---
+++ ProductInventoryApp/ProductInventoryApp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductInventoryApp.Data;
+
+namespace ProductInventoryApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all products
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
