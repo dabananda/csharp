2c577b0ed39a89e5abd49ea0a1552f73379d9a8c
dabananda <imdmitra@gmail.com>
2024-12-17 21:40:18 +0600

    Implemented Post new student

---
+++ CGPACalculatorApp/CGPACalculatorApp/Models/PostStudentReq.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CGPACalculatorApp.Models
+{
+    public class PostStudentReq
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [Required]
+        public string Session { get; set; }
+
+        [Required]
+        public string Semester { get; set; }
+
+        [Required]
+        [Range(0.0, 4.0)]
+        public double GPA { get; set; }
+
+        public double CGPA { get; set; }
+    }
+}
