df5a0d867003963a6211f4fa01dea9afba25d6cc
dabananda <imdmitra@gmail.com>
2024-12-17 22:41:57 +0600

    First commit

---
+++ ProductInventoryApp/ProductInventoryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventoryApp.Models;
+
+namespace ProductInventoryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
