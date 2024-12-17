ae36813729bf53a150b410eb83ddd9b625b51006
dabananda <imdmitra@gmail.com>
2024-12-17 16:36:41 +0600

    First Commit

---
+++ CGPACalculatorApp/CGPACalculatorApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CGPACalculatorApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CGPACalculatorApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Student> Students {  get; set; } 
+    }
+}
---
+++ CGPACalculatorApp/CGPACalculatorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CGPACalculatorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
