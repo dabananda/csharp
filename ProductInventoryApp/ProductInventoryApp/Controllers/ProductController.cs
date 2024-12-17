82f4f4b1469f170a976381cebccbdf8e464dbe71
dabananda <imdmitra@gmail.com>
2024-12-18 01:37:57 +0600

    Implemented POST product method

--- ProductInventoryApp/ProductInventoryApp/Controllers/ProductController.cs
+++ ProductInventoryApp/ProductInventoryApp/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductInventoryApp.Data;
+using ProductInventoryApp.Models;
 
 namespace ProductInventoryApp.Controllers
 {
@@ -17,6 +18,29 @@ namespace ProductInventoryApp.Controllers
         {
             var products = _context.Products.ToList();
             return View(products);
+        }
+
+        // Get "Add Product" page
+        [HttpGet]
+        public IActionResult PostProduct()
+        {
+            return View();
+        }
+
+        // Add new product
+        [HttpPost]
+        public IActionResult PostProduct(PostProductModel postProductModel)
+        {
+            var product = new Product()
+            {
+                Name = postProductModel.Name,
+                Category = postProductModel.Category,
+                Price = postProductModel.Price,
+                Quantity = postProductModel.Quantity,
+            };
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("GetProducts");
         }
     }
 }
---
+++ ProductInventoryApp/ProductInventoryApp/Models/PostProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductInventoryApp.Models
+{
+    public class PostProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
