dec487ff3bd884a5621ecbee7183690609aa6df8
dabananda <imdmitra@gmail.com>
2024-12-18 10:40:59 +0600

    Edit product methods implemented

--- ProductInventoryApp/ProductInventoryApp/Controllers/ProductController.cs
+++ ProductInventoryApp/ProductInventoryApp/Controllers/ProductController.cs
@@ -42,5 +42,41 @@ namespace ProductInventoryApp.Controllers
             _context.SaveChanges();
             return RedirectToAction("GetProducts");
         }
+
+        // Get the edit product form
+        [HttpGet]
+        public IActionResult PutProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                var updatedProduct = new PutProductModel
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Category = product.Category,
+                    Price = product.Price,
+                    Quantity = product.Quantity,
+                };
+                return View(updatedProduct);
+            }
+            return View(null);
+        }
+
+        // Update the product
+        [HttpPost]
+        public IActionResult PutProduct(PutProductModel putProductModel)
+        {
+            var existingProduct = _context.Products.Find(putProductModel.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = putProductModel.Name;
+                existingProduct.Category = putProductModel.Category;
+                existingProduct.Price = putProductModel.Price;
+                existingProduct.Quantity = putProductModel.Quantity;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("GetProducts");
+        }
     }
 }
