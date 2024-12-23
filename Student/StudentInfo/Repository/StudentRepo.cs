d2f715d65dd6cb626821a206370b5f632be52fd6
dabananda <imdmitra@gmail.com>
2024-12-23 23:45:46 +0600

    Implemented IStudentRepo in StudentRepo

---
+++ Student/StudentInfo/Repository/StudentRepo.cs
@@ -0,0 +1,32 @@
+﻿using StudentInfo.Models.Domain;
+
+namespace StudentInfo.Repository
+{
+    public class StudentRepo : IStudentRepo
+    {
+        public Task<Student> AddStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student?> DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Student>> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student?> GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student?> UpdateStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
