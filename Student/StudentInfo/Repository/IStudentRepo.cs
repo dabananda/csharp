5f3cf987c59d0bed9e441db2285a2236999ee101
dabananda <imdmitra@gmail.com>
2024-12-23 23:37:47 +0600

    Added Student repository

--- Student/StudentInfo/Repository/IStudentRepo.cs
+++ Student/StudentInfo/Repository/IStudentRepo.cs
@@ -1,6 +1,13 @@
-﻿namespace StudentInfo.Repository
+﻿using StudentInfo.Models.Domain;
+
+namespace StudentInfo.Repository
 {
     public interface IStudentRepo
     {
+        Task<IEnumerable<Student>> GetAllStudents();
+        Task<Student?> GetStudentById(int id);
+        Task<Student> AddStudent(Student student);
+        Task<Student?> UpdateStudent(int id);
+        Task<Student?> DeleteStudent(int id);
     }
 }
